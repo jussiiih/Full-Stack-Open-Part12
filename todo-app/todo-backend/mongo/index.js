541131ac37a53f0f6a6d9d44934b28dc93cf1296
jussiiih <heino.juhapekka@gmail.com>
2024-12-30 15:28:34 +0200

    mongodb url

--- todo-app/todo-backend/mongo/index.js
+++ todo-app/todo-backend/mongo/index.js
@@ -2,6 +2,8 @@ const mongoose = require('mongoose')
 const Todo = require('./models/Todo')
 const { MONGO_URL } = require('../util/config')
 
+MONGO_URL="mongodb://localhost:3456/the_database npm run dev"
+
 if (MONGO_URL && !mongoose.connection.readyState) mongoose.connect(MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
 
 
